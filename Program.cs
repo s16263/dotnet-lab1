ad48812261686c9550f53cceed572e45f576d34d
Jakub Kiedrowicz <s16263@pjwstk.edu.pl>
2026-03-10 23:46:17 +0100

    Add invalid input validation

--- Program.cs
+++ Program.cs
@@ -4,3 +4,9 @@ Console.Write("Enter a number: ");
 string? input = Console.ReadLine();
 
 Console.WriteLine($"You entered: {input}");
+
+if (!double.TryParse(input, out double number))
+{
+    Console.WriteLine("Invalid number.");
+    return;
+}
