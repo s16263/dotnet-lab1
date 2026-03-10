a322a5de0b2e8e78f8bf8d632e34a7d370179862
Jakub Kiedrowicz <s16263@pjwstk.edu.pl>
2026-03-11 00:33:25 +0100

    Update invalid number message on master

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
-﻿int[] sampleValues = { 4, 8, 2, 9, 1 };
+﻿Console.WriteLine("Simple statistics app");
+Console.Write("Enter a number: ");
+
+string? input = Console.ReadLine();
+
+Console.WriteLine($"You entered: {input}");
+
+if (!double.TryParse(input, out double number))
+{
+    Console.WriteLine("Invalid input. Please enter a number.");
+    return;
+}
+
+double result = StatisticsHelper.Square(number);
+
+Console.WriteLine($"Square: {result}");
+
+int[] sampleValues = { 4, 8, 2, 9, 1 };
+
 int minValue = StatisticsHelper.CalculateMin(sampleValues);
 Console.WriteLine($"Min: {minValue}");
