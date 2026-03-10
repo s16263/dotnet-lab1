cc194f6a75395f10c427369622853e4610355058
Jakub Kiedrowicz <s16263@pjwstk.edu.pl>
2026-03-11 00:14:40 +0100

    Use CalculateMin in Program

--- Program.cs
+++ Program.cs
@@ -1,16 +1,3 @@
-﻿Console.WriteLine("Simple statistics app");
-Console.Write("Enter a number: ");
-
-string? input = Console.ReadLine();
-
-Console.WriteLine($"You entered: {input}");
-
-if (!double.TryParse(input, out double number))
-{
-    Console.WriteLine("Invalid number.");
-    return;
-}
-
-double result = StatisticsHelper.Square(number);
-
-Console.WriteLine($"Square: {result}");
+﻿int[] sampleValues = { 4, 8, 2, 9, 1 };
+int minValue = StatisticsHelper.CalculateMin(sampleValues);
+Console.WriteLine($"Min: {minValue}");
